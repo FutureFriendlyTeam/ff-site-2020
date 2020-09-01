bd600f01e7271e5e5e9e0fb07345366c829521ba
Yasin Masukor <yasin@mentallyfriendly.com>
2020-09-02 09:42:28 +1000

    Favicons & site meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,53 @@
 const pkg = require('./package')
 
+const robots = {
+  prevent: {
+    UserAgent: '*',
+    Disallow: '/'
+  },
+  allow: {
+    UserAgent: '*',
+    Allow: '/'
+  }
+}
+
+const metaData = {
+  title:
+    'Future Friendly (Formerly Mentally Friendly) - Digital service strategy & product delivery.',
+  desc:
+    'Design & innovation team in Sydney & Canberra – we create digital services that change lives and build a better future.',
+  image: ''
+}
+
+const defaultMeta = [
+  { hid: 'charset', charset: 'utf-8' },
+  { hid: 'httpEquiv', httpEquiv: 'X-UA-Compatible', content: 'IE=edge' },
+  {
+    hid: 'viewport',
+    name: 'viewport',
+    content: 'width=device-width, initial-scale=1.0'
+  },
+  { hid: 'description', name: 'description', content: metaData.desc },
+  { hid: 'og:title', property: 'og:title', content: metaData.title },
+  { hid: 'og:type', property: 'og:type', content: 'website' },
+  { hid: 'og:image', property: 'og:image', content: metaData.image },
+  { hid: 'og:description', property: 'og:description', content: metaData.desc },
+  { hid: 'twitter:card', name: 'twitter:card', content: 'summary' },
+  { hid: 'twitter:site', name: 'twitter:site', content: '@mf_says' },
+  {
+    hid: 'twitter:title',
+    name: 'twitter:title',
+    content: 'Homepage | Mentally Friendly'
+  },
+  {
+    hid: 'twitter:description',
+    name: 'twitter:description',
+    content: metaData.desc
+  },
+  { hid: 'twitter:creator', name: 'twitter:creator', content: '@mf_says' },
+  { hid: 'twitter:image', name: 'twitter:image', content: metaData.image }
+]
+
 module.exports = {
   mode: 'spa',
 
@@ -7,13 +55,65 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+    title: metaData.title,
+    meta: defaultMeta,
+    htmlAttrs: {
+      lang: 'en-us'
+    },
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon-16x16.png'
+      },
+      { rel: 'manifest', href: '/site.webmanifest' },
+      {
+        rel: 'mask-icon',
+        href: '/safari-pinned-tab.svg',
+        color: '#00DBBF'
+      },
+      { name: 'msapplication-TileColor', content: '#ffffff' },
+      { name: 'theme-color', content: '#ffffff' },
+      {
+        rel: 'preload',
+        href: '/assets/fonts/Plain-Regular.woff',
+        as: 'font',
+        type: 'font/woff',
+        crossorigin: 'anonymous'
+      },
+      {
+        rel: 'preload',
+        href: '/assets/fonts/Plain-Medium.woff',
+        as: 'font',
+        type: 'font/woff',
+        crossorigin: 'anonymous'
+      }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    script: [
+      {
+        src:
+          'https://cdn.polyfill.io/v2/polyfill.min.js?features=IntersectionObserver'
+      },
+      {
+        src: '//js.hs-scripts.com/2273078.js',
+        id: 'hs-script-loader',
+        defer: true,
+        async: true
+      }
+    ]
   },
 
   /*
