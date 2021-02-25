477fd2691bb6192f6311b54c02a0d8791add8863
yasinMF <yasinMF@users.noreply.github.com>
2021-02-25 03:12:04 +0000

    Deploying to gh-pages from  @ 65f665c896ecb1f08a8608365868609903aa7182 🚀

---
+++ _nuxt/static/1614222713/case-studies/abc-kids/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1614222713",layout:"default",error:null,state:{brandRenameBanner:{isBannerVisible:a,isFromMfReferrer:false}},serverRendered:a,routePath:"\u002Fcase-studies\u002Fabc-kids",config:{}}}(true));
