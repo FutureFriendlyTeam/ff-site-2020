3de13ac8c667c8bb235867b5e29c35596c20c509
Yasin Masukor <yasin@futurefriendly.team>
2021-09-09 12:44:23 +1000

    Moved loading to fetch hook

--- components/bloks/CaseStudyList.vue
+++ components/bloks/CaseStudyList.vue
@@ -1,15 +1,14 @@
 <template>
   <future-div :background-color="'light'">
-
     <section id="case-studies" class="v-padding-bottom-mega">
       <div class="center-col horizontal">
         <work-block
           v-for="(story, i) in work"
-          :key="`case-study-${i}`" 
-          :story="story"/>
+          :key="`case-study-${i}`"
+          :story="story"
+        />
       </div>
     </section>
-
   </future-div>
 </template>
 
@@ -18,29 +17,26 @@ import WorkBlock from '~/components/WorkBlock.vue'
 
 export default {
   components: {
-    WorkBlock
+    WorkBlock,
   },
   props: {
     blok: {
       type: Object,
-      default: null
-    }
+      default: null,
+    },
   },
   data() {
     return {
-      work: []
+      work: [],
     }
-  },
-  mounted() {
-    this.$fetch()
   },
   async fetch() {
     return this.$storyblok
       .get(`cdn/stories/?starts_with=case-studies/`)
-      .then(res => {
+      .then((res) => {
         this.$set(this, 'work', res.data.stories)
       })
-  }
+  },
 }
 </script>
 
