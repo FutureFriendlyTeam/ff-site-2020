b77c4d5cb4759bf8a9ca23141ec0143060ed8307
yasinMF <yasinMF@users.noreply.github.com>
2021-10-27 00:54:28 +0000

    Deploying to gh-pages from  @ b092973ebb4c17706979f217c55f5337375c9aad 🚀

--- _nuxt/static/1635126664/podcasts/reports-don-t-change-the-world-farhana-yamin/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1635126664",layout:"default",error:a,state:{brandRenameBanner:{isBannerVisible:b,isFromMfReferrer:false}},serverRendered:b,routePath:"\u002Fpodcasts\u002Freports-don-t-change-the-world-farhana-yamin",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,true));
