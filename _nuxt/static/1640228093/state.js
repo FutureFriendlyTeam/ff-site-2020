de9c72dd5981c66753331dfac2f3db4da7206bc3
yasinMF <yasinMF@users.noreply.github.com>
2022-01-20 02:56:18 +0000

    Deploying to gh-pages from  @ a9c95473a2a0e0701c96ea10a1c8b36932ca0c36 🚀

--- _nuxt/static/1640228093/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1640228093",layout:"default",error:a,state:{brandRenameBanner:{isBannerVisible:b,isFromMfReferrer:false}},serverRendered:b,routePath:c,config:{_app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,true,"\u002F"));
