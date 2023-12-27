9fda510709df8fa58491e62e648d64424c78a878
liamfiddler <liamfiddler@users.noreply.github.com>
2023-12-27 12:07:26 +0000

    Deploying to gh-pages from  @ 4775cb8779c99ff8ac93ee8e48540866e2e474a3 🚀

---
+++ _nuxt/static/1703678835/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1703678835",layout:"default",error:a,state:{brandRenameBanner:{isBannerVisible:b,isFromMfReferrer:false}},serverRendered:b,routePath:c,config:{_app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,true,"\u002F"));
