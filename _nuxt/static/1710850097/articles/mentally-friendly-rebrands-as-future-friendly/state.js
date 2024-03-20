57f2a700014d4d54099c12e8565783cc2c54e522
liamfiddler <liamfiddler@users.noreply.github.com>
2024-03-20 12:08:03 +0000

    Deploying to gh-pages from  @ 4775cb8779c99ff8ac93ee8e48540866e2e474a3 🚀

--- _nuxt/static/1710850097/articles/mentally-friendly-rebrands-as-future-friendly/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1710850097",layout:"default",error:a,state:{brandRenameBanner:{isBannerVisible:b,isFromMfReferrer:false}},serverRendered:b,routePath:"\u002Farticles\u002Fmentally-friendly-rebrands-as-future-friendly",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,true));
