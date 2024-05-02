51ff86bd5db6eb57a56a14ce204dc07ef08ba6d0
liamfiddler <liamfiddler@users.noreply.github.com>
2024-05-02 12:08:18 +0000

    Deploying to gh-pages from  @ 4775cb8779c99ff8ac93ee8e48540866e2e474a3 🚀

--- _nuxt/static/1714565266/case-studies/act-inclusive-gov-services/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1714565266",layout:"default",error:a,state:{brandRenameBanner:{isBannerVisible:b,isFromMfReferrer:false}},serverRendered:b,routePath:"\u002Fcase-studies\u002Fact-inclusive-gov-services",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,true));
