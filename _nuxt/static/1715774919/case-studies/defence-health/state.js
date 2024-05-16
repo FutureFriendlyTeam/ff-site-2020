0834c53c1894b277f5e8d5bbeffe7d4235c62728
liamfiddler <liamfiddler@users.noreply.github.com>
2024-05-16 12:08:24 +0000

    Deploying to gh-pages from  @ 4775cb8779c99ff8ac93ee8e48540866e2e474a3 🚀

--- _nuxt/static/1715774919/case-studies/defence-health/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1715774919",layout:"default",error:a,state:{brandRenameBanner:{isBannerVisible:b,isFromMfReferrer:false}},serverRendered:b,routePath:"\u002Fcase-studies\u002Fdefence-health",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,true));
