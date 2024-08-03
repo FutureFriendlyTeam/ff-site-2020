9ec8d59b71e40a099f635132b6efb13234895dbb
liamfiddler <liamfiddler@users.noreply.github.com>
2024-08-03 12:07:41 +0000

    Deploying to gh-pages from  @ dcd9e8d381cd17ae26f5417a7791dc82c231902d 🚀

---
+++ _nuxt/static/1722686849/case-studies/nsw-cost-of-living/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1722686849",layout:"default",error:a,state:{brandRenameBanner:{isBannerVisible:b,isFromMfReferrer:false}},serverRendered:b,routePath:"\u002Fcase-studies\u002Fnsw-cost-of-living",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,true));
